445380fbd89eb9cfdaee03b32bfedbc1f82a62b3
sadhamusen <sadhamsafi786@gmail.com>
2023-06-27 09:37:10 +0530

    c

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,10 +16,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'bookclubtest'`, () => {
+  it(`should have as title 'bookclub'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('bookclubtest');
+    expect(app.title).toEqual('bookclub');
   });
 
   it('should render title', () => {
@@ -27,7 +27,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'bookclubtest app is running!'
+      'bookclub app is running!'
     );
   });
 });
